0b041f1a14d1dbe39c6fe49b84b05e06befe5828
ASH <sarkariresult.ash@gmail.com>
2026-04-30 03:17:11 +0530

    fix: resolve null clientPromise errors in db connection

--- src/lib/db/connection.js
+++ src/lib/db/connection.js
@@ -44,28 +44,21 @@ const CONNECTION_OPTIONS = {
   retryReads: true,
 };
 
+let client;
 let clientPromise;
 
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
-    const client = new MongoClient(MONGODB_URI, CONNECTION_OPTIONS);
-    global._mongoClientPromise = client.connect().catch(err => {
-      global._mongoClientPromise = null;
-      throw err;
-    });
+    client = new MongoClient(MONGODB_URI, CONNECTION_OPTIONS);
+    global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  if (!clientPromise) {
-    const client = new MongoClient(MONGODB_URI, CONNECTION_OPTIONS);
-    clientPromise = client.connect().catch(err => {
-      clientPromise = null;
-      throw err;
-    });
-  }
+  client = new MongoClient(MONGODB_URI, CONNECTION_OPTIONS);
+  clientPromise = client.connect();
 }
 
 export async function getClient() {
